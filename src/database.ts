61638cf885fd108ff01c9a90b2663d55c03eaf27
urmas-villem <58630256+urmas-villem@users.noreply.github.com>
2022-01-02 15:44:54 +0200

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,7 @@ import mysql from 'mysql2';
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'C4inCbw03aGwur!',
+    password: 'insert when needed',
     database: 'classes',
     waitForConnections: true,
     connectionLimit: 10,
